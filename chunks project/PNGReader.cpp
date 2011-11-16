eacf041622f1dd5f1a4c7b79f51f9ddcd551bbad
alina.pevzner <alina.pevzner@d463566c-679a-6ac2-3a4b-c447949617ad>
2011-11-16 15:46:52 +0000

    done

--- chunks project/PNGReader.cpp
+++ chunks project/PNGReader.cpp
@@ -1,16 +1,10 @@
 #include "PNGReader.h"
-
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
 #include <stdexcept>
 
 
-
-const int count_chunks_type = 20;
-const string ChunksType[] = {"IHDR", "PLTE", "IDAT", "IEND",
-                 			"bKGD", "cHRM", "gAMA", "gAMA", "hIST", "iCCP", "iTXt", "pHYs",
-                 			"sBIT", "sPLT", "sRGB", "sRGB", "tEXt", "tIME", "tRNS", "zTXt"};
 
 bool PNGReader::is_bigendian()
 {
@@ -40,108 +34,143 @@ int PNGReader::getIntInRightOrder(char *buf)
 	}
 	else
 	{
-		result = (int)buf;
+		int tmp = 0;
+		for(int i = 0; i < 4; i++)
+		{
+			if(buf[i] < 0)
+			{
+				tmp = 256 + buf[i];
+			}
+			else
+			{
+				tmp = buf[i];
+			}
+			result += tmp*pow(256, i);
+		}
 	}
 	return result;
 }
 
-int PNGReader::getIDChunk(char *buf)
+int PNGReader::getInt(char buf)
 {
-	for(int i = 0; i < count_chunks_type; i++)
+	if(buf < 0)
 	{
-		if(strcmp(buf, ChunksType[i].c_str())==0)
-		{
-			return i;
-		}
-	}	
-	return -1;
+		return (256 + buf);
+	}
+	return (int)buf;
+}
+
+unsigned int PNGReader::getIntSum(unsigned int i1, unsigned int i2)
+{
+	unsigned int res = i1 + i2;
+
+	if(res > 255)
+	{
+		return res - 256;
+	}
+	return res;
 }
 
 
 void PNGReader::getIHDRData()
 {
-	int cur_file_pos = ftell(file);
+	int cur_file_pos = file.tellg();
 	char buf[] = "0000";
 
-	if(fread(buf, 4, 1, file) != 1)
+	file.read(buf, 4);
+	if(file.gcount() != 4)
 	{
 		throw runtime_error("PNG file not read");
 	}	
-	file_width = getIntInRightOrder(buf);
+	img_width = getIntInRightOrder(buf);
 
-	if(fread(buf, 4, 1, file) != 1)
+	file.read(buf, 4);
+	if(file.gcount() != 4)
 	{
 		throw runtime_error("PNG file not read");
 	}	
-	file_heigth = getIntInRightOrder(buf);
+	img_heigth = getIntInRightOrder(buf);
 
-	if(fseek(file, cur_file_pos, SEEK_SET) != 0)
+	file.read(buf, 1);
+	if(file.gcount() != 1)
 	{
 		throw runtime_error("PNG file not read");
-	}
+	}	
+	bit_depth = getInt(buf[0]);
+
+	file.read(buf, 1);
+	if(file.gcount() != 1)
+	{
+		throw runtime_error("PNG file not read");
+	}	
+	colour_type = getInt(buf[0]);
+
+	file.read(buf, 1);
+	if(file.gcount() != 1)
+	{
+		throw runtime_error("PNG file not read");
+	}	
+	compr_method = getInt(buf[0]);
+
+	file.read(buf, 1);
+	if(file.gcount() != 1)
+	{
+		throw runtime_error("PNG file not read");
+	}	
+	filter_method = getInt(buf[0]);
+	
+	file.read(buf, 1);
+	if(file.gcount() != 1)
+	{
+		throw runtime_error("PNG file not read");
+	}	
+	interlace_method = getInt(buf[0]);
+	
+	file.seekg(cur_file_pos, ios::beg);
 }
 
 
 char* PNGReader::getIDATData()
 {
-	int cur_file_pos = ftell(file);
+	int cur_file_pos = file.tellg();
 	char *idat_tmp_data = new char[data_length];
-	if(fread(idat_tmp_data, data_length, 1, file) != 1)
+
+	file.read(idat_tmp_data, data_length);
+	if(file.gcount() != data_length)
 	{
 		throw runtime_error("PNG file not read");
 	}
-	if(fseek(file, cur_file_pos, SEEK_SET) != 0)
-	{
-		throw runtime_error("PNG file not read");
-	}
+	file.seekg(cur_file_pos, ios::beg);
+	
 	return idat_tmp_data;
 }
 
-
-PNGReader::PNGReader(char *file_name)
+void PNGReader::doInitData()
 {
-	file = fopen(file_name, "rb");
-	if(file == NULL)
-	{
-		throw runtime_error("PNG file not read");
-	}
+	file.seekg(0, ios::end);
+	img_size = file.tellg();
+	file.seekg(0, ios::beg);
 
-	if(fseek(file, 0, SEEK_END) != 0)
-	{
-		throw runtime_error("PNG file not read");
-	}
-	file_size = ftell(file);
-	if(fseek(file, 0, SEEK_SET) != 0)
-	{
-		throw runtime_error("PNG file not read");
-	}
-	file_width = 0;
-	file_heigth = 0;
+	img_width = 0;
+	img_heigth = 0;
 
 	data_length = 0;
 	char buf[] = "0000";
 	
 		
-	if(fseek(file, 8, SEEK_SET) != 0)
-	{
-		throw runtime_error("PNG file not read");
-	}		
+	file.seekg(8, ios::beg);
 		
-	while(fread(buf, 1, 4, file)==4)
+	file.read(buf, 4);
+	while(file.gcount() == 4)
 	{
 	 	data_length = getIntInRightOrder(buf);
-					
-		if(fread(buf, 4, 1, file) != 1)
+				
+		file.read(buf, 4);	
+		if(file.gcount() != 1)
 		{
 	 		throw runtime_error("PNG file not read");
 		}
-			
-		if(getIDChunk(buf) != -1)
-		{
-			v_chunks_params.push_back(ChunkParams(ChunksType[getIDChunk(buf)], data_length));
-		}
-
-			
+				
 		if(strcmp(buf, "IHDR")==0)
 		{
 			getIHDRData();
@@ -157,47 +186,30 @@ PNGReader::PNGReader(char *file_name)
 			}
 		}
 			
-			
-		if(fseek(file, data_length+4, SEEK_CUR) != 0)
-		{
-			throw runtime_error("PNG file not read");
-		}		
+		file.seekg(data_length+4, ios::cur);	
 	}		
 
-	fclose(file);
+	file.close();
+
+	isInitData = 1;
 }
 
 
-unsigned int PNGReader::getFileSize()
+PNGReader::PNGReader(char *file_name)
 {
-	return file_size;
-}
-
-unsigned int PNGReader::getFileWidth()
-{
-	return file_width;
-}
-
-unsigned int PNGReader::getFileHeigth()
-{
-	return file_heigth;
-}
-
-
-V_ChunksParams* PNGReader::getPNGChunksParams()
-{
-	V_ChunksParams *v_chunks = new V_ChunksParams();
-	for(int i = 0; i < getChunksCount(); i++)
+	file.open(file_name, fstream::binary);
+	if(file.good() != true)
 	{
-		v_chunks->push_back(ChunkParams(v_chunks_params[i].chunk_type, v_chunks_params[i].chunk_length));
+		throw runtime_error("PNG file not open");
 	}
-	
-	return v_chunks;
-}
 
+	isInitData = 0;
+}
 
 char* PNGReader::getPNGData(int &data_length)
 {
+	doInitData();
+
 	data_length = v_idat_total_data.size();
 	char *idat_data = new char[data_length];
 	for(int i = 0; i < data_length; i++)
@@ -207,8 +219,267 @@ char* PNGReader::getPNGData(int &data_length)
 	return idat_data;
 }
 
-
-int PNGReader::getChunksCount()
+int PNGReader::getIndexFilteringType(int cur_small_img)
 {
-	return v_chunks_params.size();
+	// if(interlace_method == 1)
+	// {
+	// 	if(colour_type == 2)
+	// 	{
+	// 		return 0;
+	// 	}
+	// 	throw runtime_error("PNG file has unknown data");
+	// }
+	// else
+	// {
+		return 0;
+	// }
+}
+
+void PNGReader::getSizeSmallImage(int cur_small_img, int &col, int &row)
+{
+	switch(cur_small_img)
+	{
+		case 1:
+			col = (img_width+1)/8; if((img_width+1)%8 >= 1) col++;			
+			row = (img_heigth+1)/8; if((img_heigth+1)%8 >= 1) row++;
+			break;
+
+		case 2:
+			col = (img_width+1)/8; if((img_width+1)%8 >= 5) col++;			
+			row = (img_heigth+1)/8; if((img_heigth+1)%8 >= 1) row++;
+			break;
+
+		case 3:
+			col = (img_width+1)/4; if((img_width+1)%4 >= 1) col++;
+			row = (img_heigth+1)/8; if((img_heigth+1)%8 >= 5) row++;
+			break;
+
+		case 4:
+			col = (img_width+1)/4; if((img_width+1)%4 >= 3) col++;
+			row = (img_heigth+1)/4; if((img_heigth+1)%4 >= 1) row++;
+			break;
+
+		case 5:
+			col = (img_width+1)/2; if((img_width+1)%2 >= 1) col++;
+			row = (img_heigth+1)/4; if((img_heigth+1)%4 >= 3) row++;
+			break;	
+
+		case 6:
+			col = (img_width+1)/2;
+			row = (img_heigth+1)/2; if((img_heigth+1)%2 >= 1) row++;
+			break;
+
+		case 7:
+			col = img_width;
+			row = img_heigth/2;
+			break;		
+	}
+}
+
+void doDeFilteringScanline(char *decompr_data, int filt_type, int begin, int end)
+{
+	// switch(filt_type)
+	// {
+	// 	case 0:
+	// 		if(colour_type == 2)
+	// 		{
+	// 			if(interlace_method == 0)
+	// 			{
+	// 				for(int i = begin; i <= end; i++)
+	// 				{
+	// 					image.setPixel(i-1,);
+	// 				}
+	// 			}
+	// 			else
+	// 			{
+	// 				throw runtime_error("PNG file has unknown data");
+	// 			}
+	// 		}
+	// 		else
+	// 		{
+	// 			throw runtime_error("PNG file has unknown data");
+	// 		}
+			
+			
+	// 	case 1:
+
+	// 	case 2:
+
+	// 	case 3:
+
+	// 	case 4:
+	// }
+}
+
+void PNGReader::doDeFiltering(char *decompr_data, int decompr_data_len, Image *image)
+{
+	if(filter_method != 0)
+	{
+		throw runtime_error("PNG file has incorrect data");
+	}
+
+	if(interlace_method == 0)
+	{
+		for(int i = 0; i < img_heigth; i++)
+		{
+			int index, j_count, j_delta, alfa = 0;
+			if(colour_type == 2)
+			{					
+				j_delta = 3;
+			}
+			if(colour_type == 6)
+			{
+				j_delta = 4;
+			}
+
+			j_count = img_width*j_delta;
+			index = i*(j_count + 1);
+			
+				
+			int filt_type = getInt(decompr_data[index]);
+			int j;
+			
+			switch(filt_type)
+			{
+				case 0:
+					for(j = 1; j <= j_count - j_delta + 1; j+=j_delta)
+					{
+						if(colour_type == 6)
+						{
+							alfa = getInt(decompr_data[index + j+3]);
+						}	
+
+						image->setPixel(i, j/j_delta,
+							                getInt(decompr_data[index + j]),
+							                getInt(decompr_data[index + j+1]),
+							                getInt(decompr_data[index + j+2]),
+							                alfa);
+												
+					}
+					break;
+
+				case 1:
+					if(colour_type == 6)
+					{
+						alfa = getInt(decompr_data[index + 4]);
+					}
+
+					image->setPixel(i, 0, getInt(decompr_data[index + 1]),
+						                  getInt(decompr_data[index + 2]),
+						                  getInt(decompr_data[index + 3]),
+						                  alfa);
+
+						
+
+					for(j = j_delta; j <= j_count - j_delta + 1; j+=j_delta)
+					{
+						if(colour_type == 6)
+						{
+							alfa = getIntSum(getInt(decompr_data[index + j+4]), image->getPixel(i, j/j_delta - 1).getB());
+						}
+
+						image->setPixel(i, j/j_delta,
+						                   getIntSum(getInt(decompr_data[j+1]), image->getPixel(i, j/j_delta -1 ).getR()),
+						                   getIntSum(getInt(decompr_data[j+2]), image->getPixel(i, j/j_delta - 1).getG()),
+						                   getIntSum(getInt(decompr_data[j+3]), image->getPixel(i, j/j_delta - 1).getB()),
+						                   alfa);
+					}
+						
+					break;						
+
+				case 2:		
+					if(colour_type == 6)
+					{
+						alfa = getInt(decompr_data[index + 4]);
+					}
+
+					image->setPixel(i, 0, getInt(decompr_data[index + 1]),
+						                  getInt(decompr_data[index + 2]),
+						                  getInt(decompr_data[index + 3]),
+						                  alfa);
+
+					for(j = 1; j <= j_count - j_delta + 1; j+=j_delta)
+					{
+						if(i == 0)
+						{
+							if(colour_type == 6)
+							{
+								alfa = getInt(decompr_data[index + 4]);
+							}
+
+							image->setPixel(0, j/j_delta, getInt(decompr_data[index + 1]),
+								                          getInt(decompr_data[index + 2]),
+								                          getInt(decompr_data[index + 3]),
+								                          alfa);
+						}
+						else
+						{
+							if(colour_type == 6)
+							{
+								alfa = getIntSum(getInt(decompr_data[index + j+3]), image->getPixel(i, j/j_delta - 1).getB());
+							}
+							image->setPixel(i, j/j_delta,
+							                   getIntSum(getInt(decompr_data[j]), image->getPixel(i-1, j/j_delta).getR()),
+							                   getIntSum(getInt(decompr_data[j+1]), image->getPixel(i-1, j/j_delta).getG()),
+							                   getIntSum(getInt(decompr_data[j+2]), image->getPixel(i-1, j/j_delta).getB()),
+							                   alfa);
+
+						}
+							
+					}
+					break;
+
+				case 3:
+					// for(j = 1; j <= j_count - j_delta + 1; j+=j_delta)
+					// {
+					// 	if(colour_type == 6)
+					// 	{
+					// 		alfa = getInt(decompr_data[index + 4]);
+					// 	}
+
+					// 	image->setPixel(0, j/j_delta, getInt(decompr_data[index + 1]),
+					// 			                          getInt(decompr_data[index + 2]),
+					// 			                          getInt(decompr_data[index + 3]),
+					// 			                          alfa);
+					// }
+
+					
+					
+					break;
+
+				case 4:
+						
+					break;
+			}
+		}
+
+		return;
+	}
+
+	throw runtime_error("PNG file has unknown data");		
+	
+
+	if(interlace_method == 1)
+	{
+		for(int cur_small_img = 0; cur_small_img < 7; cur_small_img++)
+		{
+			int filt_type = getInt(decompr_data[getIndexFilteringType(cur_small_img)]);
+		}
+	}
+}
+
+Image* PNGReader::getImageStruct()
+{
+	int compr_data_len = 0;
+	char *compr_data = getPNGData(compr_data_len);
+	
+	MyDecompressor decopressor = MyDecompressor(compr_data, compr_data_len);
+	int decompr_data_len = 0;
+	char *decompr_data = decopressor.getDecomprData(decompr_data_len);
+
+	Image *image = new Image(img_width, img_heigth, colour_type);
+
+	doDeFiltering(decompr_data, decompr_data_len, image);
+
+	return image;
 }
--- chunks project/PNGWorker.cpp
+++ chunks project/PNGWorker.cpp
@@ -1,9 +1,22 @@
 #include "PNGWorker.h"
 #include <stdexcept>
+#include <cstdlib>
 
-PNGWorker::PNGWorker()
+PNGWorker::PNGWorker(unsigned int img_h, unsigned int img_w)
 {
-	
+	*image = new Pixel[img_h];
+	for(int i = 0; i < img_h; i++)
+	{
+		image[i] = new Pixel[img_w];
+	}
+}
+
+PNGWorker::~PNGWorker()
+{
+	if(image != NULL)
+	{
+		delete image;
+	} 
 }
 
 int PNGWorker::getInt(char buf)
@@ -15,49 +28,39 @@ int PNGWorker::getInt(char buf)
 	return buf;
 }
 
-void PNGWorker::getSizeReducedImage(int w, int h, int numRedImg, int &col, int &row)
+void PNGWorker::getSizeReducedImage(int h, int w, int numRedImg, int &col, int &row)
 {
 	switch(numRedImg)
 	{
 		case 1:
-			col = w/8;
-			if(w%8 > 0) col++;			
-			row = h/8;
-			if(h%8 > 0) row++;
+			col = (w+1)/8; if((w+1)%8 >= 1) col++;			
+			row = (h+1)/8; if((h+1)%8 >= 1) row++;
 			break;
 
 		case 2:
-			col = w/8;
-			if(w%8 > 4) col++;			
-			row = h/8;
-			if(h%8 > 0) row++;
+			col = (w+1)/8; if((w+1)%8 >= 5) col++;			
+			row = (h+1)/8; if((h+1)%8 >= 1) row++;
 			break;
 
 		case 3:
-			col = w/4;
-			if(w%4 > 0) col++;
-			row = h/8;
-			if(h%8 > 4) row++;
+			col = (w+1)/4; if((w+1)%4 >= 1) col++;
+			row = (h+1)/8; if((h+1)%8 >= 5) row++;
 			break;
 
 		case 4:
-			col = 2*(w/8);
-			if(w%8 > 2 && w%8 < 7) col++;
-			else if(w%8 > 6) col += 2;
-			row = h/4;
-			if(h%4 > 0) row++;
+			col = (w+1)/4; if((w+1)%4 >= 3) col++;
+			row = (h+1)/4; if((h+1)%4 >= 1) row++;
 			break;
 
 		case 5:
-			col = 5*(w/8);
-			if(w%8 > 0 && w%8 < 3) col++;
-			else if(w%8 > 2 && w%8 < 5) col += 2;
-			else if(w%8 > 4 && w%8 < 7) col += 3;
-			else if(w%8 > 6) col += 4;
-			row = 2*(h/8);
-			if(h%8 > 2 && h%8 < 7) row++;
-			else if(h%8 > 6) row += 2;
+			col = (w+1)/2; if((w+1)%2 >= 1) col++;
+			row = (h+1)/4; if((h+1)%4 >= 3) row++;
 			break;	
+
+		case 6:
+			col = (w+1)/2;
+			row = (h+1)/2; if((h+1)%2 >= 1) row++;
+			break;
 
 		case 7:
 			col = w;
@@ -66,44 +69,169 @@ void PNGWorker::getSizeReducedImage(int w, int h, int numRedImg, int &col, int &
 	}
 }
 
+void getCoordPixel(int cur_red_image, int cur_row, int cur_col, int &i, int &j)
+{
+	switch(cur_red_image)
+	{
+		case 1:
+			i = 8*cur_row;     j =8*cur_col;
+			break;
+
+		case 2:
+			i = 8*cur_row + 4; j = 8*cur_col;
+			break;
+
+		case 3:
+			i = 4*cur_row;     j = 8*cur_col + 4;;
+			break;
+
+		case 4:
+			i = 4*cur_row;     j = 4*cur_col + 2;;
+			break;
+
+		case 5:
+			i = 8*cur_row;     j = 8*cur_col;
+			break;
+			
+		case 6:
+			i = 2*cur_row;     j = 2*cur_col + 1;
+			break;		
+
+		case 7:
+			i = cur_row;       j = 2*cur_col + 1;
+			break;		
+	}
+}
+
+
 char* PNGWorker::getImage(char *filt_data, int data_size, int file_w, int file_h)
 {
 	char *image = new char[file_w*file_h];
 
 	int i = 7;
 	int cur_red_image = 1;
-	while(1)
+
+	while(cur_red_image <= 7)
 	{
 		int filt_type = filt_data[i];
 
 		int col = 0, row = 0;
 		getSizeReducedImage(file_w, file_h, cur_red_image, col, row);
+
+		int cur_col = 0, cur_row = 0;
 		
-
-		switch(filt_type)
+		for(int r = 0; r < row; r++)
 		{
-			case 0:
-				for(int r = 0; r < row; r++)
+			for(int c = 0; c < col; c++)
+			{
+				int orig_r, orig_g, orig_b, orig_a;
+
+				switch(filt_type)
 				{
-					for(int c = 0; c < col; c++)
-					{
-						int orig = filt_data[i + 1 + r*col + c];
-						//setPixel(char *image, );
-					}
+					case 0:
+						orig_r = filt_data[i + cur_red_image + r*col + c];						
+						orig_g = filt_data[i + cur_red_image + r*col + c + 1];
+						orig_b = filt_data[i + cur_red_image + r*col + c + 2];
+						orig_a = filt_data[i + cur_red_image + r*col + c + 3];
+						break;
+
+					case 1:
+						int prev_r = 0, prev_g = 0, prev_b = 0, prev_a = 0;
+
+						for(int color = 0; color < 4; color++)
+						{
+							if(cur_col > 0)
+							{
+								int i = 0, j = 0;
+								getCoordPixel(cur_red_image, cur_row, cur_col-1, i, j);
+								prev = image[i][j];
+							}
+							orig = filt_data[i + cur_red_image + r*col + c] + prev;
+							}
+														
+						break;
+
+					case 2:
+						int prev_r = 0, prev_g = 0, prev_b = 0, prev_a = 0;
+						if(cur_row > 0)
+						{
+							int i = 0, j = 0;
+							getCoordPixel(cur_red_image, cur_row-1, cur_col, i, j);
+							prev = image[i][j];
+						}
+						orig = filt_data[i + cur_red_image + r*col + c] + prev;	
+						break;
+
+					case 3:
+						int prev_left = 0;
+						int prev_up = 0;
+						if(cur_col > 0)
+						{
+							int i = 0, j = 0;
+							getCoordPixel(cur_red_image, cur_row, cur_col-1, i, j);
+							prev_left = image[i][j];
+						}
+						if(cur_row > 0)
+						{
+							int i = 0, j = 0;
+							getCoordPixel(cur_red_image, cur_row-1, cur_col, i, j);
+							prev_up = image[i][j];
+						}
+						orig = filt_data[i + cur_red_image + r*col + c] + (prev_left + prev_up)/2;
+						break;
+
+					case 4:
+						int prev_left = 0;
+						int prev_up_left = 0;
+						int prev_up = 0;
+						if(cur_col > 0)
+						{
+							int i = 0, j = 0;
+							getCoordPixel(cur_red_image, cur_row, cur_col-1, i, j);
+							prev_left = image[i][j];
+						}
+						if(cur_col > 0 && cur_row > 0)
+						{
+							int i = 0, j = 0;
+							getCoordPixel(cur_red_image, cur_row-1, cur_col-1, i, j);
+							prev_up_left = image[i][j];
+						}
+						if(cur_row > 0)
+						{
+							int i = 0, j = 0;
+							getCoordPixel(cur_red_image, cur_row-1, cur_col, i, j);
+							prev_up = image[i][j];
+						}
+
+						int p = prev_left + prev_up - prev_up_left;
+    					int pa = abs(p - prev_left);
+						int pb = abs(p - prev_up);
+						int pc = abs(p - prev_up_left);
+						int pr;
+						if(pa <= pb && pa <= pc)
+						{
+						    Pr = prev_left;
+						}
+						else if(pb <= pc)
+						{
+						  	Pr = prev_up;
+						}
+						else
+						{
+						    Pr = prev_up_left;
+						}
+
+						orig = filt_data[i + cur_red_image + r*col + c] + Pr;
+						break;
 				}
-				break;
 
-			case 1:
-				break;
-
-			case 2:
-				break;
-
-			case 3:
-				break;
-
-			case 4:
-				break;
+				int i = 0;
+				int j = 0;
+				getCoordPixel(cur_red_image, cur_row, cur_col, i, j)
+				image[i][j] = orig;
+			}
 		}
+
+		cur_red_image++;
 	}
 }
